3c7fc0c6bfe260430a057017a9d6a350d1d173c8
Druseus <79805191+DRuseus@users.noreply.github.com>
2024-08-24 01:44:49 +0300

    Выполнено практическое задание по теме "Декораторы"

---
+++ Module_9_7/module_9_7.py
@@ -0,0 +1,28 @@
+def is_prime(func):
+    def wrapper(*args):
+        result = 'Простое'
+
+        func_result = func(*args)
+
+        for i in range(func_result // 2, 1, -1):
+            if func_result % i == 0 and i > 1:
+                result = 'Составное'
+                break
+        print(result)
+        return func_result
+    return wrapper
+
+
+@is_prime
+def sum_three(first, second, three):
+    return first + second + three
+
+
+result = sum_three(2, 3, 6)
+print(result)
+
+result = sum_three(10, 4, 999)
+print(result)
+
+result = sum_three(10, 5, 999)
+print(result)
